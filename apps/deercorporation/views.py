01b0832b72ec650a7bbaa4a7652d0587caee90ad
sungmuk Kang <miranaky@gmail.com>
2021-11-22 02:38:07 +0900

    FEAT: calculate fee draft
    - 회원가입/로그인 한 유저만 사용 가능
    - POST /api/v1/deer/ 요청시 사용요금 계산
    - 계산한 요금을 별도로 db 에 저장하진 않음
    - 다음 항목에 따라 계산함
        - 30분 이내 재사용시 기본요금 면제
        - 지역에 따른 기본요금과 분당 요금이 다름
        - Parkingzone 에 주차시 전체 금액 중 비율로 할인
        - 지역을 벗어나면 100m 당 추가 요금 발생
        - Forbidden Area 에 주차시 6000원 패널티 요금 부과
    - 입력값 검증
        - 시작시간이 끝나는 시간보다 앞에 있어야 함
        - latitude,longitude 는 한국 범위(33~43,124~132) 안에 있어야 함
    - GET /api/v1/deer/ 요청시 사용 히스토리를 리스트로 보여줌
        - RideHistory 를 기반으로 계산해서 보여줌

--- apps/deercorporation/views.py
+++ apps/deercorporation/views.py
@@ -1,16 +1,25 @@
 from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import RetrieveModelMixin
-from rest_framework.permissions import AllowAny
+from rest_framework.mixins import CreateModelMixin, ListModelMixin
 
 from apps.deercorporation.models import RideHistory
 from apps.deercorporation.serializers import RideHistroySerializer
 
 
-class RideHistroyView(RetrieveModelMixin, GenericAPIView):
+class RideHistroyView(CreateModelMixin, ListModelMixin, GenericAPIView):
 
     queryset = RideHistory.objects.all()
     serializer_class = RideHistroySerializer
-    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+
+    def filter_queryset(self, queryset):
+        if self.request.method == "GET":
+            queryset = queryset.filter(user=self.request.user)
+        return super().filter_queryset(queryset)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
--- apps/fees/models.py
+++ apps/fees/models.py
@@ -6,3 +6,8 @@ class DistrictFee(models.Model):
     fee_per_min = models.IntegerField()
     over_district_fee = models.IntegerField(default=200)
     district = models.OneToOneField("areas.District", on_delete=models.CASCADE)
+
+
+class ParkingZoneDiscount(models.Model):
+    discount_rate = models.FloatField(default=0.3)
+    parkingzone = models.OneToOneField("areas.ParkingZone", on_delete=models.CASCADE)
